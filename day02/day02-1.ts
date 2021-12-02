78ec3e74a7074c3c24a46707ca499ea50564b7ee
Jesús Fernández Rico <jesus.fernandezrico@aircall.io>
2021-12-02 14:46:32 +0100

    Update day02-1.ts

--- day02/day02-1.ts
+++ day02/day02-1.ts
@@ -1,19 +1,22 @@
 import path from "path";
 import { readMovementsFromFile } from "../shared/read-file";
-import { Direction } from "../shared/submarine";
+import { Direction, Movement } from "../shared/submarine";
 const movements = readMovementsFromFile(path.join(__dirname, "input.txt"));
 
-const movementCounts: Record<Direction, number> = {
-  forward: 0,
-  up: 0,
-  down: 0,
+const calculateMovements = (movements: Movement[]) => {
+  const movementCounts: Record<Direction, number> = {
+    forward: 0,
+    up: 0,
+    down: 0,
+  };
+
+  movements.forEach((movement) => {
+    movementCounts[movement.direction] += movement.position;
+  });
+  return movementCounts;
 };
 
-movements.forEach((movement) => {
-  movementCounts[movement.direction] += movement.position;
-});
-
-const result =
+const multiplyMovements = (movementCounts: Record<Direction, number>) =>
   movementCounts.forward * Math.abs(movementCounts.up - movementCounts.down);
 
-console.log(result);
+console.log(multiplyMovements(calculateMovements(movements)));
